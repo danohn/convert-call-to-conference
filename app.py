d0f1b476304dfbafe4778559659ee72e575e04c8
danohn <82357071+danohn@users.noreply.github.com>
2023-12-06 16:19:59 +1100

    Implemented the conversion logic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from twilio.twiml.voice_response import Dial, VoiceResponse
 from dotenv import load_dotenv
 import os
@@ -11,14 +11,67 @@ from_number = os.environ["TWILIO_NUMBER"]
 to_number = os.environ["DESTINATION_NUMBER"]
 
 
-@app.route("/twiml", methods=["GET", "POST"])
+@app.route("/twiml", methods=["POST"])
 def twiml_route():
     twiml = VoiceResponse()
-    dial = Dial(caller_id=from_number)
-    dial.number(to_number)
+    dial = Dial(caller_id=from_number, answer_on_bridge=True, action="/convert")
+    dial.number(
+        to_number,
+        status_callback="/number-status-callback",
+        status_callback_event="initiated ringing answered completed",
+        status_callback_method="POST",
+    )
     twiml.append(dial)
     return Response(str(twiml), mimetype="text/xml")
 
 
+@app.route("/convert", methods=["POST"])
+def convert_route():
+    parent_call_sid = request.form.get("ParentCallSid")
+    call_sid = request.form.get("CallSid")
+
+    twiml = VoiceResponse()
+    dial = Dial()
+
+    # checks if a call is a parent or child
+    # (by checking the existence of the ParentCallSid parameter)
+    if parent_call_sid:
+        dial.conference(
+            name=parent_call_sid,
+            beep=False,
+            start_conference_on_enter=True,
+            end_conference_on_exit=True,
+            jitter_buffer_size="off",
+            status_callback="/conference-status-callback",
+            status_callback_event="start end join leave",
+            status_callback_method="POST",
+        )
+        twiml.append(dial)
+        return Response(str(twiml), mimetype="text/xml")
+    else:
+        dial.conference(
+            name=call_sid,
+            beep=False,
+            start_conference_on_enter=True,
+            end_conference_on_exit=True,
+            jitter_buffer_size="off",
+            status_callback="/conference-status-callback",
+            status_callback_event="start end join leave",
+            status_callback_method="POST",
+        )
+        twiml.append(dial)
+        return Response(str(twiml), mimetype="text/xml")
+
+
+@app.route("/number-status-callback", methods=["POST"])
+def number_status_calback_route():
+    return Response(status=204)
+
+
+@app.route("/conference-status-callback", methods=["POST"])
+def conference_status_calback_route():
+    return Response(status=204)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- convert.py
+++ convert.py
@@ -8,7 +8,17 @@ account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
+base_url = os.environ["NGROK_URL"]
+
 calls = client.calls.list(status="in-progress")
 
-# calls[0] will always be the latest Call SID. Since we want to target the inbound Call SID, we need to get the 2nd latest entry
-target_sid = calls[1].sid
+# calls[0] will always be the latest Call SID (child call).
+# To convert a call from a bridged call to a conference, we need
+# to first target the child call. This will cause the parent call
+# to reach out to the action URL which should connect to the same conference
+
+target_sid = calls[0].sid
+
+call_to_update = client.calls(target_sid).update(
+    method="POST", url=f"{base_url}/convert"
+)
